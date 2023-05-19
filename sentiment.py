353b204461a7c2bc1bf4aaa5862bcc616f36fbc2
sevkibaba <sevkiyontem@gmail.com>
2023-05-19 18:14:24 +0300

    backfill structure ready

--- sentiment.py
+++ sentiment.py
@@ -1,13 +1,15 @@
-# Import the necessary libraries
 import json
+import csv
+from datetime import datetime, timezone, timedelta
 import os
+import time
 import nltk
 nltk.download('vader_lexicon')  ## Download this if you are running the script for the first time
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import openai
 import requests
 from dotenv import load_dotenv
-
+from utils import *
 load_dotenv()
 
 
@@ -15,79 +17,71 @@ class NewsCrawler:
     def __init__(self) -> None:
         # TODO:: business and tech category loop
         # TODO:: run this daily trigger discord webhook
-        self.news_api_url = 'https://newsapi.org/v2/top-headlines'
+        self.news_api_top_headlines_url = 'https://newsapi.org/v2/top-headlines'
+        self.news_api_everything_url = 'https://newsapi.org/v2/everything'
         openai.api_key = os.getenv("OPENAI_APIKEY")
         self.client = openai
 
-    def get_top_headlines(self):
-        params = {
-            # 'country': 'us',  ## try commenting this out for analyzing global headlines
-            'category': 'technology',
-            'apiKey': os.getenv("NEWSAPI_KEY"),
-            'pageSize': 100
-        }
-        response = requests.get(self.news_api_url, params=params)
+    def get_news_from_news_api(self, url, params):
+        response = requests.get(url, params=params)
+        print(response.status_code)
         data = json.loads(response.text)
         titles = []
-        for article in data['articles']:
-            titles.append(article['title'])
+        for article in data.get("articles"):
+            desc = article.get('description', '')
+            titles.append(desc if desc else '')
         return titles
 
-    def translate_titles(self, input_text):
-        prompt = f"Please translate the following text to English:\n{input_text}\nTranslation:"
-        response = self.client.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    'role': 'user',
-                    'content': prompt,
-                }
-            ]
-        )
-        summary = response.choices[0].message.content
-        return summary
-
-    def generate_summary(self, input_text):
-        prompt = f"Please summarize the following text:\n{input_text}\nSummary:"
-        response = self.client.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    'role': 'user',
-                    'content': prompt,
-                }
-            ]
-        )
-        summary = response.choices[0].message.content
-        return summary
-
     def create_sentiment_analysis(self, summary):
-
         sia = SentimentIntensityAnalyzer()
         sentiment = sia.polarity_scores(summary)
-
-        # Print the results
-        print(summary)
-        print('Sentiment Analysis Results:')
-        print('Positive:', sentiment['pos'])
-        print('Negative:', sentiment['neg'])
-        print('Neutral:', sentiment['neu'])
-        print('Compound:', sentiment['compound'])
+        # print(summary)
+        print('Compound:', sentiment['compound'], 'Positive:', sentiment['pos'], 'Negative:', sentiment['neg'], 'Neutral:', sentiment['neu'])
         if sentiment['compound'] >= 0.05:
-            return "Positive"
+            return 1
         elif sentiment['compound'] <= -0.05:
-            return "Negative"
+            return -1
         else:
-            return "Neutral"
+            return 0
 
-    def run(self):
-        titles_array = self.get_top_headlines()
-        translated_titles = self.translate_titles(' '.join(titles_array))
-        # translated_titles = self.generate_summary(translated_titles)
+    def run(self, start_date):
+        params = {
+            # 'country': 'us',  ## try commenting this out for analyzing global headlines
+            'q': 'crypto',
+            'searchIn': 'title,description',
+            'from': datetime.strftime(start_date - timedelta(days=1), '%Y-%m-%d'),
+            'to': datetime.strftime(start_date, '%Y-%m-%d'),
+            'language': 'en',
+            'sortBy': 'popularity',
+            'apiKey': os.getenv("NEWSAPI_KEY"),
+            'pageSize': 80,
+            'page': 1
+        }
+        titles_array = self.get_news_from_news_api(self.news_api_everything_url, params)
+        # translated_titles = translate_titles(self, ' '.join(titles_array))
+        translated_titles = generate_summary(self, ' '.join(titles_array))
         result = self.create_sentiment_analysis(translated_titles)
         return result
 
 
-flag = NewsCrawler().run()
-print("#" * 30 + flag + "#" * 30)
+start_date = datetime.strptime('2023-05-19', '%Y-%m-%d').replace(tzinfo=timezone.utc)
+end_date = datetime.strptime('2023-05-20', '%Y-%m-%d').replace(tzinfo=timezone.utc)
+# end_date = datetime.strptime('2023-05-19', '%Y-%m-%d').replace(tzinfo=timezone.utc)
 
+news_crawler = NewsCrawler()
+f = open('./sentiment_descriptions.csv', 'a')
+writer = csv.writer(f, delimiter=',', escapechar='', quoting=csv.QUOTE_NONE)
+
+
+while start_date < end_date:
+    flag = news_crawler.run(start_date)
+    row = []
+    row.append(datetime.strftime(start_date - timedelta(days=1), '%Y-%m-%d'))
+    row.append(flag)
+    writer.writerow(row)
+    # print("#" * 30 + flag + "#" * 30)
+    time.sleep(2)
+    start_date = start_date + timedelta(days=1)
+
+
+f.close()
