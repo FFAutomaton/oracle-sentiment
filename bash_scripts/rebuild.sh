67ae90cb8acb0cd0ede646694260d44adf9d3445
sevkibaba <sevkiyontem@gmail.com>
2023-05-19 19:32:23 +0300

    fix for deploy

--- bash_scripts/rebuild.sh
+++ bash_scripts/rebuild.sh
@@ -4,5 +4,5 @@ DOCKER_ID=$(docker ps -aqf "name=oracle-sentiment")
 docker stop $DOCKER_ID
 docker build --rm -t oracle-sentiment ./
 docker run -t -i -d --rm \
-    --name nft-mm -v /home/ubuntu/oracle-sentiment/logs:/app/logs \
-     nft-mm
+    --name oracle-sentiment -v /home/ubuntu/oracle-sentiment/logs:/app/logs \
+     oracle-sentiment
